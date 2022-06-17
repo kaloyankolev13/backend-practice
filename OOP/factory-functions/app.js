57f028de166c8710cd9ec2ba19a82bc489865b0a
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-06-17 12:14:17 +0200

    JS Classes

--- OOP/factory-functions/app.js
+++ OOP/factory-functions/app.js
@@ -26,23 +26,52 @@
 // const firstColor = makeColor(35, 255, 150);
 // let color = firstColor.hex();
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
+// COnstrictor function
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function (a = 1) {
+//   const { r, g, b } = this;
+//   return ` rgba(${r}, ${g}, ${b}, ${a})`;
+// };
+
+// const color1 = new Color(255, 40, 100);
+
+// Classes
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  rgba(a = 1.0) {
+    return `rgb(${this.innerRGB()}, ${a})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
 }
-Color.prototype.rgb = function () {
-  const { r, g, b } = this;
-  return `rgb(${r}, ${g}, ${b})`;
-};
-Color.prototype.hex = function () {
-  const { r, g, b } = this;
-  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
 
-Color.prototype.rgba = function (a = 1) {
-  const { r, g, b } = this;
-  return ` rgba(${r}, ${g}, ${b}, ${a})`;
-};
-
-const color1 = new Color(255, 40, 100);
+const c1 = new Color(255, 62, 120, 'random');
+const white = new Color(255, 255, 255, 'white');
