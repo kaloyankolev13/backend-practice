d916610953b72e99e8ae3cc8f57e124c6977b031
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-06-17 11:53:23 +0200

    constructor functions

--- OOP/factory-functions/app.js
+++ OOP/factory-functions/app.js
@@ -1,27 +1,48 @@
-function hex(r, g, b) {
-  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+// function hex(r, g, b) {
+//   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
 
-function rgb(r, g, b) {
+// function rgb(r, g, b) {
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+// rgb(244, 122, 4);
+
+// function makeColor(r, g, b) {
+//   const color = {};
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
+//   color.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   };
+//   color.hex = function () {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   };
+//   return color;
+// }
+
+// const firstColor = makeColor(35, 255, 150);
+// let color = firstColor.hex();
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
   return `rgb(${r}, ${g}, ${b})`;
-}
-rgb(244, 122, 4);
+};
+Color.prototype.hex = function () {
+  const { r, g, b } = this;
+  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
 
-function makeColor(r, g, b) {
-  const color = {};
-  color.r = r;
-  color.g = g;
-  color.b = b;
-  color.rgb = function () {
-    const { r, g, b } = this;
-    return `rgb(${r}, ${g}, ${b})`;
-  };
-  color.hex = function () {
-    const { r, g, b } = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  };
-  return color;
-}
+Color.prototype.rgba = function (a = 1) {
+  const { r, g, b } = this;
+  return ` rgba(${r}, ${g}, ${b}, ${a})`;
+};
 
-const firstColor = makeColor(35, 255, 150);
-let color = firstColor.hex();
+const color1 = new Color(255, 40, 100);
